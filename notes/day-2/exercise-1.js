8919ead91772f6c4319113f65c0baa9da00c3bc4
Levente Belteky <belteky.levente@gmail.com>
2019-02-22 13:54:00 +0100

    Add even more examples

---
+++ notes/day-2/exercise-1.js
@@ -0,0 +1,29 @@
+$('#fox-1 h1').text('<strong>Macrotis</strong>');
+$('#fox-2').css('border-color', 'blue');
+$('div').css('background-color', 'yellow');
+
+$('#fox-2').html('<strong>Macrotis</strong>');
+
+$('body').append('<div id="fox-4"><h1>Megalotis</h1></div>');
+
+let cohortName = 'Megalotis';
+
+$('body').append(
+  `
+  <div id="fox-4">
+    <h1>${cohortName}</h1>
+  </div>
+  `
+);
+
+let nevek = ['Gyuri', 'Lajos', 'Pisti'];
+nevek.forEach((elem, index) => {
+  console.log(elem);
+  $('body').append(
+    `
+    <div>
+      <h1>${elem}</h1>
+    </div>
+    `
+  );
+});
