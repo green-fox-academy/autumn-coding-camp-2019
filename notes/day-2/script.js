5f9549e264e06ee9d88a013e2450018d43663461
Viktor Rádi <rviktor87@web-fejlesztes.eu>
2019-11-21 08:00:41 +0100

    deleting previous code camp's notes

--- notes/day-2/script.js
+++
@@ -1,9 +0,0 @@
-// let tartalom = $('h1').val();
-// console.log(tartalom);
-$('button').on('click', (event) => {
-  console.log('megnyomtak.');
-  $('h1').css('color', 'tomato');
-  $('h1').html('hello');
-  $('img').fadeOut(1000);
-});
-
--- notes/day-2/todo.js
+++
@@ -1,9 +0,0 @@
-$('input[type="submit"]').on('click', (event) => {
-  event.preventDefault();
-  let todoName = $('input[type="text"]').val();
-  $('ul').append(`<li>${todoName}</li>`);
-});
-
-$('ul').on('click', (event) => {
-  $(event.target).css('color', 'red');
-});
